f3863326f1e5798d9a7f8d0b6e626851a9ca54da
liyanwei-wb <jakeli288@163.com>
2026-05-21 17:02:58 +0800

    'test'

--- src/pages/article/index.vue
+++ src/pages/article/index.vue
@@ -105,7 +105,7 @@ export default {
           const next = { ...item };
           next.tag = (next.tag || "").split(",");
           next.blogPost = removeMarkdown(next.blogPost || "");
-          next.cover = next.cover.replace(/^https?:\/\/[^/]+/, '');
+          next.cover = next.cover.replace('http://43.136.115.90:9000', '');
           return next;
         });
         this.page.total = data?.total || rawList.length || 0;
