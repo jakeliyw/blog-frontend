0d4033e44392767a1c8555503a7db34823d5595e
liyanwei-wb <jakeli288@163.com>
2026-05-21 16:55:20 +0800

    'test'

--- src/pages/article/index.vue
+++ src/pages/article/index.vue
@@ -105,7 +105,6 @@ export default {
           const next = { ...item };
           next.tag = (next.tag || "").split(",");
           next.blogPost = removeMarkdown(next.blogPost || "");
-          next.cover = next.cover.replace(/^https?:\/\/[^/]+/, '');
           return next;
         });
         this.page.total = data?.total || rawList.length || 0;
