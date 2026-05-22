5518a58399897139bf4fafd3134d84218e90c933
liyanwei-wb <jakeli288@163.com>
2026-05-22 17:10:48 +0800

    'test'

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   try {
     const { path } = req.query;
 
@@ -22,4 +22,4 @@ export default async function handler(req, res) {
   } catch (err) {
     res.status(500).send(err.message);
   }
-}
+};
