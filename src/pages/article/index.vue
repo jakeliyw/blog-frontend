374cdb6ca7a9d112d313d92bc4dc72524a10572b
liyanwei-wb <jakeli288@163.com>
2026-05-22 17:25:12 +0800

    'test'

--- src/pages/article/index.vue
+++ src/pages/article/index.vue
@@ -28,7 +28,7 @@
       </v-hover>
       <div class="text-center">
         <v-pagination
-          v-if="pageCount > 1"
+          v-if="!pageCount"
           v-model="page.current"
           :length="pageCount"
           class="pagination"
